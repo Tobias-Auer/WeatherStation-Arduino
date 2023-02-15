2cf8179b904017e8b8343bcfa9cb39f09c110e71
Tobias Auer <76256754+Tobias-Auer@users.noreply.github.com>
2023-02-15 19:29:32 +0100

    Uploaded wrong file before

--- onlineTemperature/onlineTemperature.ino
+++ onlineTemperature/onlineTemperature.ino
@@ -1,27 +1,21 @@
-// Sketch for Pico W, RP2040 Nano Connect, ESP32 and ESP8266 to fetch the Weather
-// Forecast from OpenWeather, an example from the library here:
-// https://github.com/Bodmer/OpenWeather
-
-// Sign up for a key and read API configuration info here:
-// https://openweathermap.org/
-
-// You can change the number of hours and days for the forecast in the
-// "User_Setup.h" file inside the OpenWeather library folder.
-// By default this is 6 hours (can be up to 48) and 5 days
-// (can be up to 8 days = today plus 7 days)
+//Libraries
+#include <DHT.h>
+//Constants
+#define DHTPIN 2       // what pin we're connected to
+#define DHTTYPE DHT22  // DHT 22  (AM2302)
 
 // Choose library to load
 #ifdef ESP8266
-  #include <ESP8266WiFi.h>
-  #include <WiFiClientSecure.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
 #elif defined(ARDUINO_ARCH_MBED) || defined(ARDUINO_ARCH_RP2040)
-  #if defined(ARDUINO_RASPBERRY_PI_PICO_W)
-    #include <WiFi.h>
-  #else
-    #include <WiFiNINA.h>
-  #endif
-#else // ESP32
-  #include <WiFi.h>
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+#include <WiFi.h>
+#else
+#include <WiFiNINA.h>
+#endif
+#else  // ESP32
+#include <WiFi.h>
 #endif
 
 #include <JSON_Decoder.h>
@@ -31,22 +25,27 @@
 // Just using this library for unix time conversion
 #include <Time.h>
 
+DHT dht(DHTPIN, DHTTYPE);  //// Initialize DHT sensor for normal 16mhz Arduino
+
+//Variables
+float hum;   //Stores humidity value
+float temp;  //Stores temperature value
 // =====================================================
 // ========= User configured stuff starts here =========
 // Further configuration settings can be found in the
 // OpenWeather library "User_Setup.h" file
 
-#define TIME_OFFSET 1UL * 3600UL // UTC + 0 hour
+#define TIME_OFFSET 1UL * 3600UL  // UTC + 0 hour
 // Change to suit your WiFi router
-#define WIFI_SSID     "***************"
-#define WIFI_PASSWORD "***************"
+#define WIFI_SSID "**************
+#define WIFI_PASSWORD "*************"
 
 // OpenWeather API Details, replace x's with your API key
-String api_key = "***************"; // Obtain this from your OpenWeather account
+String api_key = "********************";  // Obtain this from your OpenWeather account
 
 // Set both your longitude and latitude to at least 4 decimal places
-String latitude =  "50.11"; // 90.0000 to -90.0000 negative for Southern hemisphere
-String longitude = "8.86"; // 180.000 to -180.000 negative for West
+String latitude = "50.11";  // 90.0000 to -90.0000 negative for Southern hemisphere
+String longitude = "8.86";  // 180.000 to -180.000 negative for West
 
 String units = "metric";  // or "imperial"
 String language = "de";   // See notes tab
@@ -54,105 +53,132 @@ String language = "de";   // See notes tab
 // =========  User configured stuff ends here  =========
 // =====================================================
 
-OW_Weather ow; // Weather forecast library instance
+OW_Weather ow;  // Weather forecast library instance
 
-void setup() { 
-  Serial.begin(250000); // Fast to stop it holding up the stream
-  Serial.println("");
+void setup() {
+  Serial.begin(115200);
+  int progressbarValue = 0;
+  int tasks = 4;
 
-  Serial.printf("\n\nConnecting to %s\n", WIFI_SSID);
-  Serial.println("");
+  // ======================================================================================================================== //
+  // ====+++++++++++++++++++++++++++++++++++++++++++++RESET CONNECTION++++++++++++++++++++++++++++++++++++++++++++++++++==== //
+  // ====================================================================================================================== //
+  delay(2000);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.print("progressBar.val=0"); 
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  // ========================================================================================================================== //
+  // ====-------------------------------------------RESET CONNECTION ENDE-------------------------------------------------==== //
+  // ======================================================================================================================== //
+  // ====++++++++++++++++++++++++++++++++++++++++++START TEMPERATURESENSOR++++++++++++++++++++++++++++++++++++++++++++++==== //
+  // ====================================================================================================================== //
+  Serial.print("t0.txt=\"Temperatursensor wird gestartet...\"");
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  dht.begin();
+  Serial.print("progressBar.val=" + String(100 / tasks * 1));
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  // ========================================================================================================================== //
+  // ====-------------------------------------------START TEMPERATURSENSOR END--------------------------------------------==== //
+  // ======================================================================================================================== //
+  // ====++++++++++++++++++++++++++++++++++++++++++++++cONNECT TO WIFI++++++++++++++++++++++++++++++++++++++++++++++++++==== //
+  // ====================================================================================================================== //
+  Serial.print("t0.txt=\"Verbinde mit dem Wlan...\"");
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  // Call once for ESP32 and ESP8266
-  #if !defined(ARDUINO_ARCH_MBED)
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  #endif
-
+  int valueForTick = 0;
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    #if defined(ARDUINO_ARCH_MBED) || defined(ARDUINO_ARCH_RP2040)
-      if (WiFi.status() != WL_CONNECTED) WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    #endif
-    delay(500);
+    if (valueForTick == 360) {
+      valueForTick = 0;
+    } else {
+      valueForTick += 90;
+    }
+    Serial.print("animation.val=" + String(valueForTick));
+    Serial.write(0xFF);
+    Serial.write(0xFF);
+    Serial.write(0xFF);
+    delay(250);
   }
+  Serial.print("progressBar.val=" + String(100 / tasks * 2));
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.print("animation.val=" + String(0));
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  // ========================================================================================================================== //
+  // ====-----------------------------------------------CONNECT TO WIFI END-----------------------------------------------==== //
+  // ======================================================================================================================== //
+  // ====++++++++++++++++++++++++++++++++++++++++++++++++GET SENSOR VALUE+++++++++++++++++++++++++++++++++++++++++++++++==== //
+  // ====================================================================================================================== //
+  Serial.print("t0.txt=\"Frage Werte vom Sensor ab...\"");
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+  Serial.print("progressBar.val=" + String(100 / tasks * 3));
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  // ========================================================================================================================== //
+  // ====----------------------------------------------GET SENSOR VALUE END-----------------------------------------------==== //
+  // ======================================================================================================================== //
+  // ====++++++++++++++++++++++++++++++++++++++++++++++++++++GET TIME+++++++++++++++++++++++++++++++++++++++++++++++++++==== //
+  // ====================================================================================================================== //
 
-  Serial.println();
-  Serial.println("Connected\n");
-}
 
-void loop() {
-
-  printCurrentWeather();
-
-  // We can make 1000 requests a day
-  delay(5 * 60 * 1000); // Every 5 minutes = 288 requests per day
-}
-
-/***************************************************************************************
-**                          Send weather info to serial port
-***************************************************************************************/
-void printCurrentWeather()
-{
+  // ========================================================================================================================== //
+  // ====---------------------------------------------------GET TIME END--------------------------------------------------==== //
+  // ======================================================================================================================== //
+  // ====+++++++++++++++++++++++++++++++++++++++++GET VALUES FROM OPENWEATHERMAP++++++++++++++++++++++++++++++++++++++++==== //
+  // ====================================================================================================================== //
+  Serial.print("t0.txt=\"Frage Werte von OpenWeatherMap ab...\"");
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
   // Create the structures that hold the retrieved weather
   OW_current *current = new OW_current;
   OW_hourly *hourly = new OW_hourly;
-  OW_daily  *daily = new OW_daily;
-
-  Serial.println("\nRequesting weather information from OpenWeather... ");
+  OW_daily *daily = new OW_daily;
 
   //On the ESP8266 (only) the library by default uses BearSSL, another option is to use AXTLS
   //For problems with ESP8266 stability, use AXTLS by adding a false parameter thus       vvvvv
   //ow.getForecast(current, hourly, daily, api_key, latitude, longitude, units, language, false);
 
   ow.getForecast(current, hourly, daily, api_key, latitude, longitude, units, language);
-  Serial.println("");
-  Serial.println("Weather from Open Weather\n");
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
 
-  // Position as reported by Open Weather
-  Serial.print("Latitude            : "); Serial.println(ow.lat);
-  Serial.print("Longitude           : "); Serial.println(ow.lon);
-  // We can use the timezone to set the offset eventually...
-  Serial.print("Timezone            : "); Serial.println(ow.timezone);
-  Serial.println();
+  // ========================================================================================================================== //
+  // ====----------------------------------------GET VALUES FROM OPENWEATHERMAP END---------------------------------------==== //
+  // ======================================================================================================================== //
 
-  if (current)
-  {
-    Serial.println("############### Current weather ###############\n");
-    Serial.print("dt (time)        : "); Serial.println(strTime(current->dt));
-    Serial.print("temp             : "); Serial.println(current->temp);
-    Serial.print("feels_like       : "); Serial.println(current->feels_like);
-    Serial.print("icon             : "); Serial.println(current->icon);
-    Serial.println();
-  }
-
-  if (daily)
-  {
-    Serial.println("###############  Daily weather  ###############\n");
-    for (int i = 0; i < MAX_DAYS; i++)
-    {
-      Serial.print("Daily summary   "); if (i < 10) Serial.print(" "); Serial.print(i);
-      Serial.println();
-      Serial.print("dt (time)        : "); Serial.println(strTime(daily->dt[i]));
-      Serial.print("temp.min         : "); Serial.println(daily->temp_min[i]);
-      Serial.print("temp.max         : "); Serial.println(daily->temp_max[i]);
-
-      Serial.print("rain             : "); Serial.println(daily->pop[i]);
-      Serial.print("icon             : "); Serial.println(daily->icon[i]);
-      Serial.println();
-    }
-  }
-
-  // Delete to free up space and prevent fragmentation as strings change in length
-  delete current;
-  delete hourly;
-  delete daily;
+  Serial.print("progressBar.val=100");
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.print("t0.txt=\"DONE...\"");
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  delay(1000);
+  Serial.print("page 1");
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
 }
 
-/***************************************************************************************
-**                          Convert unix time to a time string
-***************************************************************************************/
-String strTime(time_t unixTime)
-{
-  unixTime += TIME_OFFSET;
-  return ctime(&unixTime);
-}
+void loop() {}
